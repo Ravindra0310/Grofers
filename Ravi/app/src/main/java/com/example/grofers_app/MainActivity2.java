c1c837b5c34523d28be3bdbc9be95d369b7f2725
harsh <harshraga7@gmail.com>
2021-02-04 11:08:29 +0530

    introduction.harsh

--- Ravi/app/src/main/java/com/example/grofers_app/MainActivity2.java
+++ Ravi/app/src/main/java/com/example/grofers_app/MainActivity2.java
@@ -9,10 +9,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
